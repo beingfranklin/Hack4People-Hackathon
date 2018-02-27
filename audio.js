c97d09600d86515c2e58c7442d3190860310bba3
Franklin Antony <franklinanton.007@gmail.com>
2018-02-27 19:21:22 +0530

    added node modules for audio recording

--- audio.js
+++ audio.js
@@ -1,25 +1,18 @@
-import AudioRecorder from 'audiorecorder';
-import { saveAs } from 'filesaver.js';
+import AudioRecorder from './audiorecorder.js';
+import { saveAs } from './node_modules/file-saver/FileSaver.js';
  
 let ctx = new (window.AudioContext || window.webkitAudioContext || window.mozAudioContext)();
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-audioRecorder = new AudioRecorder(ctx);
+const audioRecorder = new AudioRecorder(ctx);
 navigator.getUserMedia({ audio: true }, (e) => {
     let audioInput    = ctx.createMediaStreamSource(e);
-        
- 
     audioInput.connect(audioRecorder.node);
     audioRecorder.node.connect(ctx.destination);
     
-    
-    
-    
- 
-
     setTimeout(() => {
         audioRecorder.stop();
  
-        let recorded = audioRecorder.exportWavBlob();
+        let recorded = audioRecorder.exportWaveBlob();
         saveAs(recorded, 'recorded.wav');
     }, 5000);
  
@@ -30,12 +23,19 @@ navigator.getUserMedia({ audio: true }, (e) => {
 
 function audioStart(){
     audioRecorder.start();
-
 };
 
 function audioStop(){
     audioRecorder.stop();
 };
 
+document.getElementById('start').addEventListener('click', (event)=>{
+    audioStart();
+});
 
-  module.exports = audioStart;
+document.getElementById('stop').addEventListener('click', (event) => {
+    audioStop();
+})
+
+
+export default audioRecorder;
